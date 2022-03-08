acef47498b681f461fa44a0279ba80520b2d7ca7
Withor <dannylowithor@gmail.com>
2022-03-08 17:58:28 -0300

    first commit

---
+++ launch/sensor_fusion.launch.py
@@ -0,0 +1,27 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    ekf = Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[os.path.join(get_package_share_directory("sensor_fusion"), 'params', 'ekf.yaml')]
+           )
+
+
+    return LaunchDescription([
+
+        ekf
+    ])
